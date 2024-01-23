611f381d4c424dfd10de0da9b82eaaa52d426439
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-01-23 15:25:41 -0300

    feat: create struct product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,28 @@
-import { useRouter } from "next/router"
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/src/styles/pages/product";
 
 export default function Product() {
-  const {query} = useRouter()
+  return (
+    <>
+      <ProductContainer>
+        <ImageContainer></ImageContainer>
 
-  return(
-    <h1>Product {JSON.stringify(query)}</h1>
-  )
+        <ProductDetails>
+          <h1>Camiseta X</h1>
+          <span>RS 78.90</span>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+            tempore dicta amet saepe quam voluptate distinctio eligendi dolor
+            beatae neque.
+          </p>
+
+          <button>Comprar agora</button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
+  );
 }
